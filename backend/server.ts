4cf43f26b9ab762d0ca26ab9e83eb86ed0bd10bb
Said Mounaim <saidmounaim00@gmail.com>
2022-01-26 19:36:33 +0100

    Setup Project

---
+++ backend/server.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { Application, Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app: Application = express();
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 5000;
+
+// Default 
+app.get("/api", (req: Request, res: Response)  => {
+    res.status(201).json({ message: "Welcome to Hotel Booking App" });
+})
+
+app.listen(PORT, (): void => console.log(`Server is running on PORT ${PORT}`));
