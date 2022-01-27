7dd91625fdceb8e2e8f70638a98ead24f8b40e59
Said Mounaim <saidmounaim00@gmail.com>
2022-01-27 19:36:46 +0100

    Hashing Password

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 export interface IUser extends mongoose.Document {
 
@@ -40,6 +41,21 @@ const UserSchema = new mongoose.Schema({
 }, {
     timestamps: true
 });
+
+UserSchema.pre("save", async function(next) {
+
+    const user = this as IUser;
+
+    if(!user.isModified("password")) return next();
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(user.password, salt);
+
+    user.password = hash;
+
+    next();
+
+})
 
 const User = mongoose.model<IUser>("User", UserSchema);
 
