580d84035f14cad9a45f651f18ddbbff515a8d7c
Said Mounaim <saidmounaim00@gmail.com>
2022-01-29 18:11:02 +0100

    Setup Frontend With React TypeScript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello From Frontend</h1>
+    </div>
+  );
+}
+
+export default App;
