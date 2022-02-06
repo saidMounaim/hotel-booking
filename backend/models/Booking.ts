ba658d92bb22567b1a545c1e8196f651967f4f7b
Said Mounaim <saidmounaim00@gmail.com>
2022-02-06 10:52:34 +0100

    Add Booking Model

---
+++ backend/models/Booking.ts
@@ -0,0 +1,77 @@
+import mongoose from 'mongoose';
+
+type TPaymentInfo = {
+    id: string,
+    status: string,
+}
+
+export interface IBooking {
+    room: string,
+    user: string,
+    checkInDate: Date,
+    checkOutDate: Date,
+    amountPaid: Number,
+    daysOfStay: Number,
+    paymentInfo: TPaymentInfo,
+    paidAt: Date,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+const BookingSchema = new mongoose.Schema({
+
+    room: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "Room"
+    },
+
+    user: {
+        type: mongoose.Types.ObjectId,
+        required: true,
+        ref: "User"
+    },
+
+    checkInDate: {
+        type: Date,
+        required: true,
+    },
+
+    checkOutDate: {
+        type: Date,
+        required: true,
+    },
+
+    amountPaid: {
+        type: Number,
+        required: true,
+    },
+
+    daysOfStay: {
+        type: Number,
+        required: true,
+    },
+
+    paymentInfo: {
+        id: {
+            type: String,
+            required: true,
+        },
+        status: {
+            type: String,
+            required: true,
+        }
+    },
+
+    paidAt: {
+        type: Date,
+        required: true
+    }
+
+}, {
+    timestamps: true
+})
+
+const Booking = mongoose.model<IBooking>("Booking", BookingSchema);
+
+export default Booking;
